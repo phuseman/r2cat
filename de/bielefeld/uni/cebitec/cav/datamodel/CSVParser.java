0a33d52ae8e8d8b040bfc2ee1557b07f5ccf8f8d
Peter Husemann <phuseman@users.noreply.github.com>
2008-09-18 15:00:06 +0000

    the parser is now generic, such that blat output can be parsed too

--- de/bielefeld/uni/cebitec/cav/datamodel/CSVParser.java
+++ de/bielefeld/uni/cebitec/cav/datamodel/CSVParser.java
@@ -32,9 +32,20 @@ import java.io.IOException;
  */
 
 public class CSVParser {
-	static final String delimiter = "\t";
+	private String delimiter = "\t";
 
 	BufferedReader input;
+	
+	private int queryIdField=0;
+	private int queryLengthField=1;
+	private int targetIdField=2;
+	private int targetLengthField=3;
+	private int queryStartField=4;
+	private int queryEndField=5;
+	private int targetStartField=6;
+	private int targetEndField=7;
+	
+	private String mode="none";
 
 	public CSVParser(File file) {
 		try {
@@ -45,10 +56,9 @@ public class CSVParser {
 	}
 
 	/**
-	 * Parses the contents of a csv file into a AlignmentPositionsList.<br>
-	 * Tab is expected as field delimiter. Lines starting with # are ignored.
-	 * <br>
-	 * The parsed fields are expected at the following positions:
+	 * Sets the parsing mode compatible to the swift output (with -compassemb).
+	 *  Tab is expected as field delimiter.
+	 * 	The parsed fields are expected at the following positions:
 	 * <br>0 Id/Name of the query/contig sequence
 	 * <br>1 Length of the query/contig sequence
 	 * <br>2 Id/Name of the target/reference sequence
@@ -57,6 +67,63 @@ public class CSVParser {
 	 * <br>9 query end position of this match
 	 * <br>10 target start position of this match
 	 * <br>11 target end position of this match
+	 */
+	public void setSwiftMode() {
+		queryIdField=0;
+		queryLengthField=1;
+		targetIdField=2;
+		targetLengthField=3;
+		queryStartField=8;
+		queryEndField=9;
+		targetStartField=10;
+		targetEndField=11;
+		delimiter = "\t";
+		
+		mode="swift";
+	}
+	
+	/**
+	 * Sets the parsing mode compatible to the swift output (with -compassemb).
+	 *  Tab is expected as field delimiter.
+	 * 	The parsed fields are expected at the following positions:
+	 * <br>9 Id/Name of the query/contig sequence
+	 * <br>10 Length of the query/contig sequence
+	 * <br>13 Id/Name of the target/reference sequence
+	 * <br>14 Length of the target/reference sequence
+	 * <br>11 query start position of this match
+	 * <br>12 query end position of this match
+	 * <br>15 target start position of this match
+	 * <br>16 target end position of this match
+	 */
+	public void setBlatMode() {
+		queryIdField=9;
+		queryLengthField=10;
+		targetIdField=13;
+		targetLengthField=14;
+		queryStartField=11;
+		queryEndField=12;
+		targetStartField=15;
+		targetEndField=16;
+		delimiter = "\t";
+		mode="blat";
+	}
+	
+	/**
+	 * Parses the contents of a csv file into a AlignmentPositionsList.<br>
+	 * By default is tab expected as field delimiter. Lines starting with # are ignored.
+	 * <br>
+	 * The parsed fields are expected at the following positions:
+	 * <br>0 Id/Name of the query/contig sequence
+	 * <br>1 Length of the query/contig sequence
+	 * <br>2 Id/Name of the target/reference sequence
+	 * <br>3 Length of the target/reference sequence
+	 * <br>4 query start position of this match
+	 * <br>5 query end position of this match
+	 * <br>6 target start position of this match
+	 * <br>7 target end position of this match
+	 * <br>
+	 * <br> the order can be changes with the presets setSwiftMode or setBlatMode.
+	 * 
 	 * @return
 	 */
 	public AlignmentPositionsList parse() {
@@ -64,6 +131,7 @@ public class CSVParser {
 		String[] tokens;
 		int lineNumber = 1;
 		int errors = 0;
+		StringBuilder errorlines=new StringBuilder();
 
 		AlignmentPositionsList apl = new AlignmentPositionsList();
 
@@ -88,11 +156,11 @@ public class CSVParser {
 
 				try {
 
-					String queryId = tokens[0];
-					long queryLength = Long.parseLong(tokens[1]);
+					String queryId = tokens[queryIdField];
+					long queryLength = Long.parseLong(tokens[queryLengthField]);
 
-					String targetId = tokens[2];
-					long targetLength = Long.parseLong(tokens[3]);
+					String targetId = tokens[targetIdField];
+					long targetLength = Long.parseLong(tokens[targetLengthField]);
 
 					DNASequence query = null;
 					DNASequence target = null;
@@ -111,11 +179,21 @@ public class CSVParser {
 						target = new DNASequence(targetId, targetLength);
 					}
 
-					long queryStart = Long.parseLong(tokens[8]);
-					long queryEnd = Long.parseLong(tokens[9]);
+					long queryStart = Long.parseLong(tokens[queryStartField]);
+					long queryEnd = Long.parseLong(tokens[queryEndField]);
 
-					long targetStart = Long.parseLong(tokens[10]);
-					long targetEnd = Long.parseLong(tokens[11]);
+					if( mode.equals("blat")) {
+						//blat has a special flag for reverse hits.
+						//we expect that queryStart>queryEnd for reversed hits.
+						if(tokens[8].matches("-")) {
+							queryStart=queryLength-queryStart;
+							queryEnd=queryLength - queryEnd;
+						}
+					}
+
+					
+					long targetStart = Long.parseLong(tokens[targetStartField]);
+					long targetEnd = Long.parseLong(tokens[targetEndField]);
 
 					AlignmentPosition ap = new AlignmentPosition(target,
 							targetStart, targetEnd, query, queryStart, queryEnd);
@@ -123,6 +201,7 @@ public class CSVParser {
 
 				} catch (NumberFormatException e) {
 					errors++;
+					errorlines.append(lineNumber + " ");
 				}
 			}
 		} catch (IOException e) {
@@ -135,9 +214,9 @@ public class CSVParser {
 
 		if (errors > 0) {
 			System.err.println(this.getClass().getName() + ": There were "
-					+ errors + " parse errors!");
+					+ errors + " parse errors! Lines: " + errorlines.toString());
 			System.err
-					.println("long integers are expected at positions 1,3,8-11");
+					.println(String.format("long integers are expected at positions %d, %d, %d and %d", queryLengthField,targetLengthField,queryStartField,queryEndField,targetStartField,targetEndField));
 		}
 		
 		if (apl.size()>0) {
@@ -145,6 +224,14 @@ public class CSVParser {
 		} else {
 			return null;
 		}
+	}
+
+	public String getDelimiter() {
+		return delimiter;
+	}
+
+	public void setDelimiter(String delimiter) {
+		this.delimiter = delimiter;
 	}
 
 }
