f3de97c5a404b3e82d4037404bf14b6a765b1b46
Anica Seidel <aseidel@users.noreply.github.com>
2010-10-26 11:07:20 +0000

    26.10.2010 - ASE: add Border Class, Panel for the Contigs and created first Window

---
+++ de/bielefeld/uni/cebitec/contigadjacencygraph/gui/ChooseContigPanel.java
@@ -0,0 +1,122 @@
+package de.bielefeld.uni.cebitec.contigadjacencygraph.gui;
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+
+public class ChooseContigPanel extends JPanel {
+
+	private ContigBorder border;
+	private ContigBorder reverseBorder;
+	private ContigBorder repeatBorder;
+	private ContigBorder reverseRepeatBorder;
+	
+	public ChooseContigPanel() {
+		
+		GroupLayout layout = new GroupLayout(this);
+		
+		setLayout(layout);
+		
+		border = new ContigBorder(false , false);
+		reverseBorder = new ContigBorder(false, true);
+		repeatBorder = new ContigBorder(true, false);
+		reverseRepeatBorder = new ContigBorder(true, true);
+		
+
+
+		JButton leftContig1 = new JButton();
+		JButton leftContig2 = new JButton();
+		JButton leftContig3 = new JButton();
+		JButton leftContig4 = new JButton();
+		JButton leftContig5 = new JButton();
+
+		JButton centralContig = new JButton();
+
+		JButton rightContig1 = new JButton();
+		JButton rightContig2 = new JButton();
+		JButton rightContig3 = new JButton();
+		JButton rightContig4 = new JButton();
+		JButton rightContig5 = new JButton();
+		
+		leftContig1.setText("Contig 1");
+		leftContig1.setContentAreaFilled(false);
+		leftContig1.setBorder(border);
+		leftContig2.setText("Contig 2");
+		leftContig2.setContentAreaFilled(false);
+		leftContig2.setBorder(repeatBorder);
+		leftContig3.setText("Contig 3");
+		leftContig3.setBorder(border);
+		leftContig3.setContentAreaFilled(false);
+		leftContig4.setText("Contig 4");
+		leftContig4.setBorder(border);
+		leftContig4.setContentAreaFilled(false);
+		leftContig5.setText("Contig 5");
+		leftContig5.setBorder(reverseBorder);
+		leftContig5.setContentAreaFilled(false);
+		
+		centralContig.setText("aktuelles Contig");
+		centralContig.setBorder(reverseRepeatBorder);
+		centralContig.setContentAreaFilled(false);
+		
+		rightContig1.setText("Contig 1");
+		rightContig1.setContentAreaFilled(false);
+		rightContig1.setBorder(reverseBorder);
+		rightContig2.setText("Contig 2");
+		rightContig2.setContentAreaFilled(false);
+		rightContig2.setBorder(border);
+		rightContig3.setText("Contig 3");
+		rightContig3.setContentAreaFilled(false);
+		rightContig3.setBorder(border);
+		rightContig4.setText("Contig 4");
+		rightContig4.setContentAreaFilled(false);
+		rightContig4.setBorder(repeatBorder);
+		rightContig5.setText("Contig 5");
+		rightContig5.setContentAreaFilled(false);
+		rightContig5.setBorder(border);
+		/*
+		 * automatic gaps that correspond to preferred distances between
+		 * neighboring components (or between a component and container border)
+		 */
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+
+		layout.setHorizontalGroup(layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
+						.addComponent(leftContig1).addComponent(leftContig2)
+						.addComponent(leftContig3).addComponent(leftContig4)
+						.addComponent(leftContig5))
+				.addComponent(centralContig)
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
+						.addComponent(rightContig1).addComponent(	rightContig2)
+						.addComponent(rightContig3).addComponent(rightContig4)
+						.addComponent(rightContig5))
+				);
+
+		layout.setVerticalGroup(layout.createSequentialGroup()
+				.addGroup(
+						layout.createParallelGroup(
+								GroupLayout.Alignment.BASELINE).addComponent(leftContig1)
+								.addComponent(rightContig1))
+				.addGroup(
+						layout.createParallelGroup(
+								GroupLayout.Alignment.BASELINE).addComponent(
+								leftContig2).addComponent(rightContig2))
+				.addGroup(
+						layout.createParallelGroup(
+								GroupLayout.Alignment.BASELINE).addComponent(
+								leftContig3).addComponent(centralContig)
+								.addComponent(rightContig3))
+				.addGroup(
+						layout.createParallelGroup(
+								GroupLayout.Alignment.BASELINE).addComponent(
+								leftContig4).addComponent(rightContig4))
+				.addGroup(
+						layout.createParallelGroup(
+								GroupLayout.Alignment.BASELINE).addComponent(
+								leftContig5).addComponent(rightContig5)));
+	}
+}
---
+++ de/bielefeld/uni/cebitec/contigadjacencygraph/gui/ContigBorder.java
@@ -0,0 +1,151 @@
+package de.bielefeld.uni.cebitec.contigadjacencygraph.gui;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.RenderingHints;
+import java.awt.geom.GeneralPath;
+
+import javax.swing.border.AbstractBorder;
+
+public class ContigBorder extends AbstractBorder{
+
+	private boolean isRepeat;
+	private boolean isReverse;
+	
+	public ContigBorder(boolean isRepeat, boolean isReverse ) {
+		this.isRepeat = isRepeat;
+		this.isReverse = isReverse;
+	}
+
+	public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+	        
+	    Color oldColor = g.getColor();     
+		Graphics2D g2 = (Graphics2D) g;
+		
+		GeneralPath p;
+		if(isRepeat ==false && isReverse == false){
+			p = makeContig(0,0, c.getWidth(),c.getHeight());
+			g2.setColor(Color.BLUE);
+			g2.draw(p);
+		}
+		if(isRepeat == true && isReverse == false){
+			p = makeRepeatContig(0,0, c.getWidth(),c.getHeight());
+			g2.setColor(Color.RED);
+			g2.draw(p);
+		}
+		if(isRepeat == false && isReverse == true){
+			p = makeReverseContig(0,0, c.getWidth(),c.getHeight());
+			g2.setColor(Color.GREEN);
+			g2.draw(p);
+		}
+		if(isRepeat == true && isReverse == true){
+			p = makeReverseRepeatContig(0,0, c.getWidth(),c.getHeight());
+			g2.setColor(Color.MAGENTA);
+			g2.draw(p);
+		}
+		//g2.fill(p);
+		
+		g.setColor(oldColor);
+	}
+
+	/*
+	 * (x, y + height)------------(x + width, y + height) (x + width + 10, y +
+	 * height/2) Startpunkt *(x, y)-------------(x + width, y)
+	 */
+	private  GeneralPath makeContig(int x, int y, int width, int height) {
+
+		GeneralPath p = new GeneralPath();
+
+		p.moveTo(x, y); // Startpunkt
+		p.lineTo(x + width - 10, y); // 1
+		p.lineTo(x + width, y + height / 2); // 2
+		p.lineTo(x + width -10, y + height -1); // 3
+		p.lineTo(x, y + height -1); // 4
+		p.closePath();
+
+		return p;
+	}
+	
+	private GeneralPath makeRepeatContig(int x, int y, int width, int height) {
+		
+		GeneralPath p = new GeneralPath();
+		
+		p.moveTo(x, y + 5); // Startpunkt
+		p.lineTo(x + width - 15, y + 5); // 1
+		p.lineTo(x + width - 5, y + height / 2); // 2
+		p.lineTo(x + width -15, y + height  -1); // 3
+		p.lineTo(x, y + height -1); // 4
+		p.closePath();
+		p.moveTo(x +2, y+5);
+		p.lineTo(x+3, y+3);
+		p.lineTo(x+width-15, y+3);
+		p.lineTo(x+width-3, y+height/2 );
+		p.lineTo(x+width-12, y+height-2);
+		p.lineTo(x+width-13, y+height-2);
+		p.moveTo(x + 3, y+3);
+		p.lineTo(x +4, y+1);
+		p.lineTo(x+width-15, y+1);
+		p.lineTo(x+width, y+height/2 );
+		p.lineTo(x+width-9, y+height-4);
+		p.lineTo(x+width-12, y+height-4);
+		return p;
+	}
+	private GeneralPath makeReverseRepeatContig(int x, int y, int width, int height) {
+		
+		GeneralPath p = new GeneralPath();
+		
+		p.moveTo(x+15, y+height-1);
+		p.lineTo(x +4, y + height/2);
+		p.lineTo(x+15, y + 4);
+		p.lineTo(x + width -1, y + 4);
+		p.lineTo(x + width-1, y+height-1);
+		p.closePath();
+		p.moveTo(x+15, y+height-2);
+		p.lineTo(x+13, y+height-2);
+		p.lineTo(x+2, y+height/2);
+		p.lineTo(x+15, y+2);
+		p.lineTo(x+width-4, y+2);
+		p.lineTo(x+width-1, y+4);
+		p.moveTo(x+13, y+height-4);
+		p.lineTo(x+11, y+height-4);
+		p.lineTo(x, y+height/2);
+		p.lineTo(x+15, y);
+		p.lineTo(x+width-6, y);
+		p.lineTo(x+width-4, y-2);
+		
+		return p;
+	}
+	
+	/*
+	 * 							 	2------------3
+	 * 						1						
+	 * 			Startpunkt *|5-------------4
+	 */
+	private GeneralPath makeReverseContig(int x, int y, int width, int height) {
+		
+		GeneralPath p = new GeneralPath();
+		
+		p.moveTo(x + 10, y);
+		p.lineTo(x +1, y + height/2);
+		p.lineTo(x + 10, y + height -1);
+		p.lineTo(x + width - 1, y + height -1);
+		p.lineTo(x + width - 1, y);
+		p.closePath();
+		return p;
+}
+
+	public Insets getBorderInsets(Component c){
+		return new Insets(10, 10 ,10, 10);
+	}
+	
+	public Insets getBorderInsets(Component c, Insets insets){
+		return insets;
+	}
+
+	public boolean isBorderOpaque() {
+		return true;
+	}
+
+}
